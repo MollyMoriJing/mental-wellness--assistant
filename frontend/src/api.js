b0eab56a1614ee8d7a72a84a411e843742e29009
DJ <du.jing2@northeastern.edu>
2025-10-07 02:53:53 -0400

    VITE_API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
+  baseURL: `${import.meta.env.VITE_API_URL || 'http://localhost:5001'}/api`,
+  headers: { 'Content-Type': 'application/json' }
+  })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
